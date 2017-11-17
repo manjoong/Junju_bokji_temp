61d90fe55af0925e29b4fac5c4b54a6f3b46d398
skekf123@naver.com <skekf123@naver.com>
2017-11-17 21:58:49 +0900

    icon update

--- app/src/main/java/com/likelion/manjoong/oa/WelfareSearch.java
+++ app/src/main/java/com/likelion/manjoong/oa/WelfareSearch.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
@@ -23,22 +24,23 @@ public class WelfareSearch extends Activity {
     String key="8rULtNWhTcB9%2FM2fsJHpyZqt%2FK94LqQWfNl9QTmvLKP56PotRCl8iO2yOaYnxffLg7nG8qHsMPLIijmqenTyrg%3D%3D";
     //key = 공공데이터 api 사용을 위한 고유 키
     String data;  //공공 데이터를 가져올 데이타를 스트링 값으로 선언
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welfare_search);
-
-        edit= (EditText)findViewById(R.id.edit); //검색창 선언
+        LinearLayout woman = (LinearLayout)findViewById(R.id.woman);
+        LinearLayout child = (LinearLayout)findViewById(R.id.child);
+        LinearLayout oldman = (LinearLayout)findViewById(R.id.oldman);
+        LinearLayout studen = (LinearLayout)findViewById(R.id.student);
+        LinearLayout handicapped = (LinearLayout)findViewById(R.id.handicapped);
         text= (TextView)findViewById(R.id.result); //보여줄 창 선언
-    }
 
-    //Button을 클릭했을 때 자동으로 호출되는 callback method....
-    public void mOnClick(View v){
-
-        switch( v.getId() ){
-            case R.id.button:
-
+        woman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str = "여성";
                 //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
                 new Thread(new Runnable() {
 
@@ -61,10 +63,125 @@ public class WelfareSearch extends Activity {
                     }
                 }).start();
 
-                break;
-        }
 
-    }//mOnClick method..
+            }
+        });
+
+        child.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               str = "유아";
+                //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+
+                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
+                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+
+                    }
+                }).start();
+
+            }
+        });
+        oldman.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               str = "노인";
+                //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+
+                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
+                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+
+                    }
+                }).start();
+
+            }
+        });
+        studen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str = "청소년";
+                //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+
+                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
+                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+
+                    }
+                }).start();
+
+            }
+        });
+        handicapped.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                str = "장애인";
+                //Android 4.0 이상 부터는 네트워크를 이용할 때 반드시 Thread 사용해야 함
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data= getXmlData();//아래 메소드를 호출하여 XML data를 파싱해서 String 객체로 얻어오기
+
+                        //UI Thread(Main Thread)를 제외한 어떤 Thread도 화면을 변경할 수 없기때문에
+                        //runOnUiThread()를 이용하여 UI Thread가 TextView 글씨 변경하도록 함
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data); //TextView에 문자열  data 출력
+                            }
+                        });
+
+                    }
+                }).start();
+
+            }
+        });
+
+    }
+
 
 
     //XmlPullParser를 이용하여 제공하는 OpenAPI XML 파일 파싱하기(parsing)
@@ -72,7 +189,7 @@ public class WelfareSearch extends Activity {
 
         StringBuffer buffer=new StringBuffer();
 
-        String str= edit.getText().toString();//EditText에 작성된 Text얻어오기
+
         String search = URLEncoder.encode(str);//한글의 경우 인식이 안되기에 utf-8 방식으로 encoding..
         String query = "%EC%A0%84%EB%A0%A5%EB%A1%9C";
 
@@ -103,7 +220,12 @@ public class WelfareSearch extends Activity {
                         tag= xpp.getName();//테그 이름 얻어오기
 
                         if(tag.equals("list")) ;// 첫번째 검색결과
-
+                        else if(tag.equals("welFareName")){
+                            buffer.append("복지명 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());// 요소의 TEXT 읽어와서 문자열버퍼에 추가
+                            buffer.append("\n"); //줄바꿈 문자 추가
+                        }
                         else if(tag.equals("welFareChargeTel")){
                             buffer.append("기관 전화번호 : ");
                             xpp.next();
@@ -170,12 +292,6 @@ public class WelfareSearch extends Activity {
                             buffer.append(xpp.getText());// 요소의 TEXT 읽어와서 문자열버퍼에 추가
                             buffer.append("\n"); //줄바꿈 문자 추가
                         }
-                        else if(tag.equals("welFareName")){
-                            buffer.append("복지명 :");
-                            xpp.next();
-                            buffer.append(xpp.getText());// 요소의 TEXT 읽어와서 문자열버퍼에 추가
-                            buffer.append("\n"); //줄바꿈 문자 추가
-                        }
                         else if(tag.equals("welFarePurpose")){
                             buffer.append("복지목적 :");
                             xpp.next();
@@ -221,8 +337,6 @@ public class WelfareSearch extends Activity {
     }//getXmlData method....
 
 }//MainActivity class..
-
-
 
 
 
